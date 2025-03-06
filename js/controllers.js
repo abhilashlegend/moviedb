295669eaacc8dfb72d0964074f52356dcbf06c2a
abhilashlegend <abhilash456a@live.com>
2025-03-06 21:20:40 +0530

    Added pages

--- js/controllers.js
+++ js/controllers.js
@@ -10,13 +10,26 @@ app.controller('moviesController', ['$scope', function($scope) {
 
 app.controller('recommendedMoviesController', ['$scope','recommendedMoviesService', function($scope, recommendedMoviesService) {
 
-     $scope.movies = null;
-    recommendedMoviesService.getRecommendedMovies().then(result => {
-        $scope.movies = result.data.results;
-        console.log(result);
-    }).catch(error => {
-        console.log(error);
-    })
+     $scope.movies = []; // Initialize movies array
+     $scope.pages =  Array.from({ length: 100}, (_, i) => i + 1); // Create an array of numbers 1 to 100;
+     $scope.selectedPage = 1; // Default to page 1
+     
+     // Reusable function for fetching recommended movies
+     function fetchRecommendedMovies(page){
+        recommendedMoviesService.getRecommendedMovies(page).then(result => {
+            $scope.movies = result.data.results; // Update movies in scope
+        }).catch(error => {
+            console.error("Error fetching movies:", error); 
+        });
+     }
+
+     $scope.onPageChange = function() {
+        fetchRecommendedMovies($scope.selectedPage);
+     }
+    
+     // Initial API call to fetch movies for the default page
+    fetchRecommendedMovies($scope.selectedPage);
+
 }])
 
 app.controller('showsController', ['$scope', function($scope) {
--- js/services.js
+++ js/services.js
@@ -1,8 +1,11 @@
 app.service('recommendedMoviesService',["$http", function($http) {
 
-    const apiurl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc`;
+    
 
-    this.getRecommendedMovies = function(){
+    this.getRecommendedMovies = function(Pgno){
+
+        const apiurl = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${Pgno}&sort_by=popularity.desc`;
+
         return $http({
             method: 'GET',
             url: apiurl,
